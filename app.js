8de3c910e923ace4880b82e45159c31aca0b33e9
Gheorghe <madangheorghe.30@gmail.com>
2025-02-20 15:55:22 +0100

    consegna es

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = 3000;
 // importo il router dei posts
@@ -7,15 +8,18 @@ const postsRouter = require('./routers/posts.js')
 // definisco la cartella dei file statici 
 app.use(express.static('public'));
 
+app.use(cors(
+    { origin: 'http://localhost:5173' }
+))
 // definisco la rotta home
-app.get('/', (req, res)=>{
-res.send('Questo è il server dei miei post') 
+app.get('/', (req, res) => {
+    res.send('Questo è il server dei miei post')
 });
 
 // Utilizziamo la rotta delle pizze  definendo la parte iniziale
 app.use("/posts", postsRouter)
 
 // il port 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`il server è in ascolto sul port: ${port}`);
 });
